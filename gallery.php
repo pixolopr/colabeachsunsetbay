8c4541d8b8922127661e8fad2b393c9a92ba1b5d
pixolo <abhay.pixolo@gmail.com>
2020-10-07 12:50:30 +0530

    lastest server files

---
+++ gallery.php
@@ -0,0 +1,329 @@
+<!doctype html>
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cola Beach - Gallery</title>
+
+    <script type="text/javascript">
+        window._wpemojiSettings = {
+            "baseUrl": "",
+            "ext": ".png",
+            "svgUrl": "",
+            "svgExt": ".svg",
+            "source": {
+                "concatemoji": ""
+            }
+        };
+        ! function (a, b, c) {
+            function d(a) {
+                var c, d, e, f, g, h = b.createElement("canvas"),
+                    i = h.getContext && h.getContext("2d"),
+                    j = String.fromCharCode;
+                if (!i || !i.fillText) return !1;
+                switch (i.textBaseline = "top", i.font = "600 32px Arial", a) {
+                case "flag":
+                    return i.fillText(j(55356, 56806, 55356, 56826), 0, 0), !(h.toDataURL().length < 3e3) && (i.clearRect(0, 0, h.width, h.height), i.fillText(j(55356, 57331, 65039, 8205, 55356, 57096), 0, 0), c = h.toDataURL(), i.clearRect(0, 0, h.width, h.height), i.fillText(j(55356, 57331, 55356, 57096), 0, 0), d = h.toDataURL(), c !== d);
+                case "diversity":
+                    return i.fillText(j(55356, 57221), 0, 0), e = i.getImageData(16, 16, 1, 1).data, f = e[0] + "," + e[1] + "," + e[2] + "," + e[3], i.fillText(j(55356, 57221, 55356, 57343), 0, 0), e = i.getImageData(16, 16, 1, 1).data, g = e[0] + "," + e[1] + "," + e[2] + "," + e[3], f !== g;
+                case "simple":
+                    return i.fillText(j(55357, 56835), 0, 0), 0 !== i.getImageData(16, 16, 1, 1).data[0];
+                case "unicode8":
+                    return i.fillText(j(55356, 57135), 0, 0), 0 !== i.getImageData(16, 16, 1, 1).data[0];
+                case "unicode9":
+                    return i.fillText(j(55358, 56631), 0, 0), 0 !== i.getImageData(16, 16, 1, 1).data[0]
+                }
+                return !1
+            }
+
+            function e(a) {
+                var c = b.createElement("script");
+                c.src = a, c.type = "text/javascript", b.getElementsByTagName("head")[0].appendChild(c)
+            }
+            var f, g, h, i;
+            for (i = Array("simple", "flag", "unicode8", "diversity", "unicode9"), c.supports = {
+                    everything: !0,
+                    everythingExceptFlag: !0
+                }, h = 0; h < i.length; h++) c.supports[i[h]] = d(i[h]), c.supports.everything = c.supports.everything && c.supports[i[h]], "flag" !== i[h] && (c.supports.everythingExceptFlag = c.supports.everythingExceptFlag && c.supports[i[h]]);
+            c.supports.everythingExceptFlag = c.supports.everythingExceptFlag && !c.supports.flag, c.DOMReady = !1, c.readyCallback = function () {
+                c.DOMReady = !0
+            }, c.supports.everything || (g = function () {
+                c.readyCallback()
+            }, b.addEventListener ? (b.addEventListener("DOMContentLoaded", g, !1), a.addEventListener("load", g, !1)) : (a.attachEvent("onload", g), b.attachEvent("onreadystatechange", function () {
+                "complete" === b.readyState && c.readyCallback()
+            })), f = c.source || {}, f.concatemoji ? e(f.concatemoji) : f.wpemoji && f.twemoji && (e(f.twemoji), e(f.wpemoji)))
+        }(window, document, window._wpemojiSettings);
+    </script>
+    <style type="text/css">
+        img.wp-smiley,
+        img.emoji {
+            display: inline !important;
+            border: none !important;
+            box-shadow: none !important;
+            height: 1em !important;
+            width: 1em !important;
+            margin: 0 .07em !important;
+            vertical-align: -0.1em !important;
+            background: none !important;
+            padding: 0 !important;
+        }
+    </style>
+    <link rel='stylesheet' id='contact-form-7-css' href='css/styles.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='woocommerce-layout-css' href='css/woocommerce-layout.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='woocommerce-smallscreen-css' href='css/woocommerce-smallscreen.css' type='text/css' media='only screen and (max-width: 768px)' />
+    <link rel='stylesheet' id='woocommerce-general-css' href='css/woocommerce.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='js_composer_front-css' href='css/js_composer.min.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='ebor-body-font-css' href='http://fonts.googleapis.com/css?family=Open+Sans%3A400%2C500%2C600' type='text/css' media='all' />
+    <link rel='stylesheet' id='ebor-heading-font-css' href='http://fonts.googleapis.com/css?family=Raleway%3A100%2C400%2C300%2C500%2C600%2C700' type='text/css' media='all' />
+    <link rel='stylesheet' id='ebor-lato-font-css' href='http://fonts.googleapis.com/css?family=Lato%3A300%2C400' type='text/css' media='all' />
+    <link rel='stylesheet' id='ebor-bootstrap-css' href='css/bootstrap.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='ebor-plugins-css' href='css/plugins.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='ebor-fonts-css' href='css/fonts.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='ebor-theme-styles-css' href='css/theme.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='font-awesome-css' href='css/font-awesome.min.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='ebor-style-css' href='css/style.css' type='text/css' media='all' />
+    <style id='ebor-style-inline-css' type='text/css'>
+        ul[data-bullet] {
+            list-style: none;
+        }
+        
+        .image-square > .content {
+            -ms-transform: translateY(-50%);
+        }
+        
+        .third-thumbs li {
+            width: 25%;
+            height: 25vw;
+            border: 1px solid white;
+        }
+        
+        .custom-forms .radio-option {
+            width: 100px;
+        }
+    </style>
+    <script type='text/javascript' src='js/jquery.js'></script>
+    <script type='text/javascript' src='js/jquery-migrate.js'></script>
+
+    <noscript>
+        <style type="text/css">
+            .wpb_animate_when_almost_visible {
+                opacity: 1;
+            }
+        </style>
+    </noscript>
+</head>
+
+<body class="page page-id-125 page-child parent-pageid-981 page-template-default normal-layout parallax-3d btn-regular custom-forms wpb-js-composer js-comp-ver-4.12.1 vc_responsive" style="overflow-x: hidden;">
+
+
+    <div class="nav-container">
+
+        <nav>
+
+
+
+            <div class="nav-utility">
+
+                <div class="module left">
+                    <i class="ti-location-arrow">&nbsp;</i>
+                    <span class="sub">Cola Beach Sunset Bay, Matimol, Canacona, Goa</span>
+                </div>
+
+                <div class="module left">
+                    <i class="ti-email">&nbsp;</i>
+                    <span class="sub">colabeachsunsetbay@gmail.com</span>
+                </div>
+                
+                <div class="module right close-msg">
+                    <span class="sub">Not Operational due to the Covid Pandemic. Stay Safe !</span>
+                </div>
+
+
+
+            </div>
+            <div class="nav-bar">
+
+                <div class="module left">
+                    <a href="index.html">
+                        <img class="logo logo-light" src="img/logo-light.png" />
+                        <img class="logo logo-dark" src="img/logo-dark.png" />
+                    </a>
+                </div>
+
+                <div class="module widget-handle mobile-toggle right visible-sm visible-xs">
+                    <i class="ti-menu"></i>
+                </div>
+
+                <div class="module-group right">
+
+                    <div class="module left">
+                        <ul id="menu-standard-navigation" class="menu">
+                            <li id="menu-item-966" class="menu-item menu-item-type-custom menu-item-object-custom  menu-item-966"><a href="index.html">Home</a>
+                            </li>
+                            <li id="menu-item-967" class="menu-item menu-item-type-custom menu-item-object-custom  menu-item-967"><a href="about.html">About</a>
+                            </li>
+                            <li id="menu-item-897" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-897"><a href="gallery.php" style="color: #0759ef;">Gallery</a>
+                            </li>
+                            <li id="menu-item-964" class="menu-item menu-item-type-custom menu-item-object-custom  menu-item-964"><a href="inquire.html">Inquire</a>
+                            </li>
+                            <li id="menu-item-965" class="menu-item menu-item-type-custom menu-item-object-custom  menu-item-965"><a href="tariff.html">Tariff</a>
+                            </li>
+                            <li id="menu-item-963" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-963"><a href="contact.html">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+
+        </nav>
+
+    </div>
+
+    <div class="main-container">
+
+        <div class="ebor-page-wrapper">
+            <a id="home" class="in-page-link" href="#"></a>
+
+            <div class="row light-wrapper  light-wrapper normal-padding  light-wrapper normal-padding">
+                <div class="wpb_column vc_column_container col-md-12">
+                    <div class="vc_column-inner ">
+                        <div class="wpb_wrapper">
+                            <div class="wpb_text_column wpb_content_element ">
+                                <div class="wpb_wrapper">
+                                    <h4 class="uppercase mb16" style="text-align: center;">COLA BEACH SUNSET BAY</h4>
+                                    <p class="lead mb64" style="text-align: center;">In person views are always better.</p>
+
+                                </div>
+                            </div>
+
+                            <div class="wpb_text_column wpb_content_element ">
+                                <div class="wpb_wrapper">
+                                    <div class="lightbox-grid third-thumbs bg-dark">
+                                        <ul>
+                                            <?php $x=1; while($x <=48) { ?>
+                                            <li>
+                                                <a href="img/gallery/photo (<?php echo $x; ?>).jpg" data-lightbox="true">
+                                                    <span class="background-image-holder">
+	    	    	            <img width="1024" height="534" src="img/gallery/photo (<?php echo $x; ?>).jpg" class="background-image" alt="cola"  sizes="(max-width: 1024px) 100vw, 1024px" />
+	    	    	        </span>
+                                                </a>
+                                            </li>
+                                            <?php $x++; } ?>
+                                        </ul>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <footer class="footer-1 bg-dark">
+
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div id="text-3" class="widget widget_text">
+                            <div class="textwidget"><img alt="Logo" class="logo" src="img/logo-light.png">
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div class="clear"></div>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-sm-6">
+                        <span class="sub">
+                	<a href="http://www.pixoloproductions.com/" target="_self">Copyright © 2017 Pixolo Productions</a>
+                   </span>
+                    </div>
+
+                    <div class="col-sm-6 text-right">
+                        <ul class="list-inline social-list">
+                            <li>
+                                <a href="https://www.facebook.com/colabeachsunsetbay/" target="_blank">
+                                    <i class="fa fa-facebook-square" aria-hidden="true"></i> Follow us on Facebook
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <a class="btn btn-sm fade-half back-to-top inner-link" href="#top">Top</a>
+
+        </footer>
+
+    </div>
+    <!--/body-wrapper-->
+
+    <script type='text/javascript' src='js/jquery.form.min.js'></script>
+    <script type='text/javascript'>
+        /* <![CDATA[ */
+        var _wpcf7 = {
+            "loaderUrl": "",
+            "recaptcha": {
+                "messages": {
+                    "empty": "Please verify that you are not a robot."
+                }
+            },
+            "sending": "Sending ...",
+            "cached": "1"
+        };
+        /* ]]> */
+    </script>
+    <script type='text/javascript' src='js/script.js'></script>
+    <script type='text/javascript' src='js/jquery.blockUI.min.js'></script>
+    <script type='text/javascript'>
+        /* <![CDATA[ */
+        var woocommerce_params = {
+            "ajax_url": "\/wp-admin\/admin-ajax.php",
+            "wc_ajax_url": "\/pages\/services-2\/?wc-ajax=%%endpoint%%"
+        };
+        /* ]]> */
+    </script>
+    <script type='text/javascript' src='js/woocommerce.min.js'></script>
+    <script type='text/javascript' src='js/jquery.cookie.min.js'></script>
+    <script type='text/javascript'>
+        /* <![CDATA[ */
+        var wc_cart_fragments_params = {
+            "ajax_url": "\/wp-admin\/admin-ajax.php",
+            "wc_ajax_url": "\/pages\/services-2\/?wc-ajax=%%endpoint%%",
+            "fragment_name": "wc_fragments"
+        };
+        /* ]]> */
+    </script>
+    <script type='text/javascript' src='js/cart-fragments.min.js'></script>
+    <script type='text/javascript' src='js/bootstrap.min.js'></script>
+    <script type='text/javascript' src='js/plugins.js'></script>
+    <script type='text/javascript' src='js/parallax.js'></script>
+    <script type='text/javascript'>
+        /* <![CDATA[ */
+        var wp_data = {
+            "access_token": "2158990778.1e524cd.776a5c36583f42528a0403cd56cf7bd9",
+            "client_id": "1e524cded7bb43459ed5d91d448beec3",
+            "hero_animation": "fade",
+            "hero_autoplay": "false",
+            "hero_timer": "3000"
+        };
+        /* ]]> */
+    </script>
+    <script type='text/javascript' src='js/scripts.js'></script>
+    <script type='text/javascript' src='js/comment-reply.min.js'></script>
+    <script type='text/javascript' src='js/wp-embed.min.js'></script>
+    <script type='text/javascript' src='js/js_composer_front.min.js'></script>
+</body>
+
+</html>
